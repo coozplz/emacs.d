1ea2765c0e6e30e21b24adfff388ee860beac45d
oskwon <oskwon@inka.co.kr>
2016-12-28 16:26:30 +0900

    Modify init-local file
    - Add kill ring to system clipboard
    - Comment to neotree
    - Powerline configuration
    - elfeed for feedly

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -34,6 +34,10 @@
 ;; ----------------------------------------------------------------------------
 (windmove-default-keybindings 'meta)
 
+
+;; ----------------------------------------------------------------------------
+;; Easier transition between windows
+;; ----------------------------------------------------------------------------
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
 (load-theme 'monokai t)
@@ -65,6 +69,63 @@
   (shell-command-on-region
    (point-min) (point-max)
    (read-shell-command "Shell command on buffer: ")))
+
+
+;;-------------------------------------------------------------------------------
+;; kill ring to system clipboard
+;;-------------------------------------------------------------------------------
+(setq *is-a-mac* (eq system-type 'darwin))
+(setq *cygwin* (eq system-type 'cygwin) )
+(setq *linux* (or (eq system-type 'gnu/linux) (eq system-type 'linux)) )
+(defun copy-to-x-clipboard ()
+  (interactive)
+  (if (region-active-p)
+      (progn
+        (cond
+         ((and (display-graphic-p) x-select-enable-clipboard)
+          (x-set-selection 'CLIPBOARD (buffer-substring (region-beginning) (region-end))))
+         (t (shell-command-on-region (region-beginning) (region-end)
+                                     (cond
+                                      (*cygwin* "putclip")
+                                      (*is-a-mac* "pbcopy")
+                                      (*linux* "xsel -ib")))
+            ))
+        (message "Yanked region to clipboard!")
+        (deactivate-mark))
+    (message "No region active; can't yank to clipboard!")))
+
+(defun paste-from-x-clipboard()
+  (interactive)
+  (cond
+   ((and (display-graphic-p) x-select-enable-clipboard)
+    (insert (x-get-selection 'CLIPBOARD)))
+   (t (shell-command
+       (cond
+        (*cygwin* "getclip")
+        (*is-a-mac* "pbpaste")
+        (t "xsel -ob"))
+       1))
+   ))
+
+(defun my/paste-in-minibuffer ()
+  (local-set-key (kbd "M-y") 'paste-from-x-clipboard)
+  )
+
+(add-hook 'minibuffer-setup-hook 'my/paste-in-minibuffer)
+
+
+;;-------------------------------------------------------------------------------
+;; Powerline configuration
+;;-------------------------------------------------------------------------------
+(require 'powerline)
+(powerline-center-theme)
+
+;;-------------------------------------------------------------------------------
+;; elfeed for feedly
+;;-------------------------------------------------------------------------------
+(setq-default elfeed-search-filter "@1-week-ago +unread ")
+
+
 
 
 (provide 'init-local)
